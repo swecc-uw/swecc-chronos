bd247ef5d794e69cdfb7e40978f274fb759dfa06
hoanghohohaha <hoanghohohaha@gmail.com>
2024-12-30 07:34:23 -0800

    clean up

--- app/main.py
+++ app/main.py
@@ -1,19 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.endpoints import router as api_router
-from app.utils.scheduler import lifespan, scheduler
-
-def ticker():
-    print("Tick")
-
-scheduler.add_job_every(ticker, "s", 2, job_id="ticker")
-
 
 app = FastAPI(
     title="Docker Stats API",
     description="API for monitoring Docker container statistics",
     version="1.0.0",
-    lifespan=lifespan
 )
 
 app.add_middleware(
